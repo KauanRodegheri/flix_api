716c3a2dd9bf405de479ce11e0ce663e0f0291b0
Kauan Rodegheri <kauan.rodegheri@gmail.com>
2024-10-30 13:47:37 -0300

    teste do gitignore

--- genres/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from genres.models import Genre
-
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
-    search_fields = ('id', 'name',)
-
