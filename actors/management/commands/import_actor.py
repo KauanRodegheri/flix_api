f4d6bc10dc235a626c551ad78c4cc7920167d018
Kauan Rodegheri <kauan.rodegheri@gmail.com>
2024-12-09 15:08:55 -0300

    comando no django command atualizado

--- actors/management/commands/import_actor.py
+++ actors/management/commands/import_actor.py
@@ -3,6 +3,7 @@ import csv
 from actors.models import Actor
 from datetime import datetime
 
+
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
@@ -19,20 +20,37 @@ class Command(BaseCommand):
             csv_reader = csv.DictReader(file)
             actor = Actor.objects.all()
             list_actors_repeat = []
+            linha = int(input('qual linha deseja começar? '))
+            cont = 0
             for row in csv_reader:
-                try:
-                    if actor.filter(name=row['name']).exists():
-                        list_actors_repeat.append(row['name'])
-                    else:
-                        actor.create(
-                            name=row['name'],
-                            birthday=datetime.strptime(row['birthday'], '%Y-%m-%d').date(),
-                            nationality=row['nationality']
-                        )
-                        self.stdout.write(self.style.SUCCESS(f'ator {row['name']} cadastrado'))
-                except Exception as error:
-                    print(f'houve um erro no salvamento {error}')
+                cont += 1
+                if cont >= linha - 1:
+                    try:
+                        if actor.filter(name=row['name'], birthday=row['birthday']).exists():
+                            while True:
+                                opc = input(f'o ator {row['name']} ja existe, deseja inserir mesmo assim?').lower()
+                                match opc:
+                                    case "n":
+                                        list_actors_repeat.append(row['name'])
+                                        break
+                                    case "s":
+                                        self.create_actor(actor, row['name'], row['birthday'], row['nationality'])
+                                        break
+                                    case _:
+                                        continue
+                        else:
+                            self.create_actor(actor, row['name'], row['birthday'], row['nationality'])
+                            self.stdout.write(self.style.SUCCESS(f'ator {row['name']} cadastrado'))
+                    except Exception as error:
+                        print(f'houve um erro no salvamento {error}')
         if list_actors_repeat:
             self.stdout.write(self.style.NOTICE(f'actors exists in database: {list_actors_repeat}'))
         else:
             self.stdout.write(self.style.SUCCESS('Todos atores cadastrados com sucesso'))
+
+    def create_actor(self, queryset, name, birthday, nationality):
+        queryset.create(
+            name=name,
+            birthday=datetime.strptime(birthday, '%Y-%m-%d').date(),
+            nationality=nationality
+        )
